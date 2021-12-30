e718706c23b893c61ed7716ff94de295f1492f67
Matt Horner <mhorner7@gmail.com>
2021-12-30 11:03:39 -0600

    Add batch script to run through relays to verify the program is working.

---
+++ testrelays.bat
@@ -0,0 +1,11 @@
+
+:while
+relay_ft245r.exe r 1 s on
+relay_ft245r.exe r 1 s off
+relay_ft245r.exe r 2 s on
+relay_ft245r.exe r 2 s off
+relay_ft245r.exe r 3 s on
+relay_ft245r.exe r 3 s off
+relay_ft245r.exe r 4 s on
+relay_ft245r.exe r 4 s off
+goto :while
